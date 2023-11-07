06029651d02fc7ea9eb33f78a11793e4eaec166a
kesic2004 <kesic2004_com@163.com>
2023-11-07 02:52:39 -0500

    20231107 commit

--- src/main/java/com/model/special/entity/Special.java
+++ src/main/java/com/model/special/entity/Special.java
@@ -2,9 +2,11 @@ package com.model.special.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import java.io.Serializable;
 
+@TableName("special")
 public class Special extends Model<Special> {
    private static final long serialVersionUID = 1L;
    @TableId(
