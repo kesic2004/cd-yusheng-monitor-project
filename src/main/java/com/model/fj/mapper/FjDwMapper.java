d98b8ab45ca0abbbdad93e48889c36ce86c6a453
kesic2004 <kesic2004_com@163.com>
2023-11-07 02:46:14 -0500

    20231107 commit

--- src/main/java/com/model/fj/mapper/FjDwMapper.java
+++ src/main/java/com/model/fj/mapper/FjDwMapper.java
@@ -2,9 +2,10 @@ package com.model.fj.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.model.fj.entity.FjDw;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 public interface FjDwMapper extends BaseMapper<FjDw> {
    @Select({"Select id, stationname, platformurl, token, jblstationcode, bz from fj_dw where jblstationcode=#{jblCode} limit 1"})
-   FjDw findDwByJblCode(String jblCode);
+   FjDw findDwByJblCode(@Param("jblCode") String jblCode);
 }
