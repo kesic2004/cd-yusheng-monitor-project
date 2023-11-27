b58b08192e144160ae7f5b6a8d78080d28ce15ab
kesic2004 <kesic2004_com@163.com>
2023-11-27 08:46:49 -0500

    20231127 commit

--- src/main/java/com/model/tblclient/entity/Tblclient.java
+++ src/main/java/com/model/tblclient/entity/Tblclient.java
@@ -2,9 +2,11 @@ package com.model.tblclient.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import java.io.Serializable;
 
+@TableName("")
 public class Tblclient extends Model<Tblclient> {
    private static final long serialVersionUID = 1L;
    @TableId(
